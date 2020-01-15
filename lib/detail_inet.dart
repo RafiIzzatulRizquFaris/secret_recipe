a3d854eeefa55ade5398e3fe8bfe211111ed12c6
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-01-15 21:54:02 +0700

    Create detail_inet.dart

---
+++ lib/detail_inet.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreenInet extends StatelessWidget{
+  DetailScreenInet({Key key, this.item, this.item2, this.item3}) : super(key: key);
+  final String item;
+  final String item2;
+  final String item3;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Detail : $item',
+          style: TextStyle(
+              color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.black87,
+        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () => Navigator.pop(context, false)),
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Card(
+                  color: Colors.black,
+                  elevation: 2.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                  ),
+                  child: Hero(
+                      tag: item,
+                      child: Image.network(
+                        item2,
+                        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress){
+                          if(loadingProgress == null) return child;
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null ?
+                              loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes : null,
+                            ),
+                          );
+                        },
+                      )
+//                      FadeInImage.assetNetwork(placeholder: item2, image: item2)
+                  ),
+                  margin: EdgeInsets.only(right: 20.0, left: 20.0),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 5, bottom: 5),
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  item,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                margin: EdgeInsets.only(left: 5, right: 5),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Instructions :",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        margin: EdgeInsets.all(0),
+                        color: Colors.black,
+                        padding: EdgeInsets.all(5.0),
+                        child: Card(
+                          color: Colors.white30,
+                          elevation: 5.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0))
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              item3,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
