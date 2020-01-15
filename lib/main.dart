08a4e450ac7897c617dbde0fb20edca47f468fa1
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-01-15 21:53:08 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,117 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:secret_recipe/fragment/breakfast_fragment.dart';
+import 'package:secret_recipe/fragment/desert_fragment.dart';
+import 'package:secret_recipe/fragment/meal_fragment.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  List<String> data = ['Chicken','Salmon','Beef','Pork','Avocado','Apple Cider Vinegar','Asparagus'];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bahan Makanan',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Bahan Makanan'),
-        ),
-        body: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index){
-            return ListTile(
-              title: Text(data[index]),
-              onTap: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(item: data[index])
-                    )
-                );
-              },
-            );
-          },
-        )
-      )
+      home: MainPage(),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget{
-  DetailScreen({Key key, this.item}) : super(key: key);
-  final String item;
+class MainPage extends StatefulWidget{
+  @override
+  _MainPageState createState() =>_MainPageState();
+}
+
+class _MainPageState extends State<MainPage>{
+
+  final List<Widget> _children = [BreakfastScreen(), DesertScreen(), MealScreen()];
+  String title = 'Breakfast';
+  int page = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail Bahan Makanan'),
-        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () => Navigator.pop(context, false)),
+        backgroundColor: Colors.black,
+        elevation: 2,
+        centerTitle: true,
+        title: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+          ),
+        ),
       ),
-      body: Center(
-        child: Text(item),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 50.0,
+        index: 0,
+        items: <Widget>[
+          Container(
+            child: Icon(Icons.free_breakfast, size: 30,color: Colors.white,),
+            padding: EdgeInsets.all(5),
+          ),
+          Container(
+            child: Icon(Icons.fastfood, size: 30, color: Colors.white,),
+            padding: EdgeInsets.all(5),
+          ),
+          Container(
+            child: Icon(Icons.add_comment, size: 30, color: Colors.white,),
+            padding: EdgeInsets.all(5),
+          )
+        ],
+        color: Color.fromRGBO(48, 46, 46, 1.0),
+        buttonBackgroundColor: Color.fromRGBO(48, 46, 46, 1.0),
+        backgroundColor: Colors.black,
+        animationCurve: Curves.easeInOutCirc,
+        animationDuration: Duration(milliseconds: 500),
+        onTap: (index){
+          switch (index){
+            case 0:
+              setState(() {
+                page = 0;
+                title = "Breakfast";
+              });
+              break;
+            case 1:
+              setState(() {
+                page = 1;
+                title = "Dessert";
+              });
+              break;
+            case 2:
+              setState(() {
+                page = 2;
+                title = "Other Meals";
+              });
+              break;
+          }
+        },
       ),
+      body: _children[page],
     );
   }
 }
+
+//showSnackBar(BuildContext context, Meal meal) {
+//  String strMeal = meal.title;
+//  final snackBar = SnackBar(
+//    content: Text('Do you want to see $strMeal details ?'),
+//    action: SnackBarAction(
+//      label: 'Yes',
+//      onPressed: () {
+//        Navigator.push(
+//            context,
+//            PageRouteBuilder(
+//              transitionDuration: Duration(milliseconds: 800),
+//              pageBuilder: (BuildContext context, Animation<double> animation,
+//                  Animation<double> secondaryAnimation) =>
+//                  DetailScreen(item: meal.title, item2: meal.image, item3: meal.desc,),
+//            ));
+//      },
+//    ),
+//  );
+//  Scaffold.of(context).showSnackBar(snackBar);
+//}
